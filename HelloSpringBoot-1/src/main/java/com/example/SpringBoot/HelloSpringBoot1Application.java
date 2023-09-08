490454cb496a392db2e23d9ee567f92604d8160b
VpLearningJava <vivek.3189@gmail.com>
2023-09-08 18:09:37 +0530

    first file

---
+++ HelloSpringBoot-1/src/main/java/com/example/SpringBoot/HelloSpringBoot1Application.java
@@ -0,0 +1,13 @@
+package com.example.SpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpringBoot1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloSpringBoot1Application.class, args);
+	}
+
+}
---
+++ HelloSpringBoot-1/src/test/java/com/example/SpringBoot/HelloSpringBoot1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringBoot1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
