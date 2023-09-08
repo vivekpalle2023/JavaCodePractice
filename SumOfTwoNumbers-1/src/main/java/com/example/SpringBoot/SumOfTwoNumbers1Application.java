ca517f0dc836039e78218992f31dd6e64674fff1
VpLearningJava <vivek.3189@gmail.com>
2023-09-08 18:14:58 +0530

    fifth commit

---
+++ SumOfTwoNumbers-1/src/main/java/com/example/SpringBoot/SumOfTwoNumbers1Application.java
@@ -0,0 +1,13 @@
+package com.example.SpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SumOfTwoNumbers1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SumOfTwoNumbers1Application.class, args);
+	}
+
+}
