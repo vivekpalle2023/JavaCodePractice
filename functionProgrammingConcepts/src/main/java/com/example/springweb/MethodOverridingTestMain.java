570e296d55c8ed849e226b7cf4eea1711c190a1d
VpLearningJava <vivek.3189@gmail.com>
2023-09-08 18:16:11 +0530

    seventh commit

---
+++ functionProgrammingConcepts/src/main/java/com/example/springweb/MethodOverridingTestMain.java
@@ -0,0 +1,14 @@
+package com.example.springweb;
+
+public class MethodOverridingTestMain {
+	public static void main(String[] args) {
+		
+		String s1="Test";
+		String s2=new String("Test");
+		System.out.println(s1==s2);
+		System.out.println(s1.equals(s2));
+	}
+
+}
+
+
---
+++ functionProgrammingConcepts/src/main/java/com/example/springweb/Sample.java
@@ -0,0 +1,61 @@
+package com.example.springweb;
+
+import java.util.List;
+import java.util.Map;
+import java.util.ArrayList;
+import java.util.TreeMap;
+
+
+
+public class Sample {
+	
+	public static TreeMap<String, String> AssigningShift(int x, List<String> y){
+		
+        int morning=1;
+        int afternoon=2;
+        int night=2;
+        int intialCount=0;
+		TreeMap<String, String> assigningShift=new TreeMap<String, String>(); 
+
+		while(intialCount<x) {
+			
+			while(morning!=0){
+				assigningShift.put(y.get(intialCount), "Morning");
+                 morning--;
+				intialCount++;
+			}
+			while(afternoon!=0){
+				assigningShift.put(y.get(intialCount), "AfterNoon");
+				afternoon--;
+				intialCount++;
+			}
+			while(night!=0){
+				assigningShift.put(y.get(intialCount), "Night");
+				night--;
+				intialCount++;
+			}
+		
+			}
+		return assigningShift;
+		
+	}
+	
+	public static void main(String[] args) { 
+	
+	
+		List<String> a=new ArrayList<>();
+		a.add("Swaroop");
+		a.add("Pavan");
+		a.add("Sateesh");
+		a.add("Firoz");
+		a.add("Sindhuja");
+		int count=(int) a.stream().count();
+      
+	 
+		for(Map.Entry m:  AssigningShift(count,a).entrySet()) {
+		System.out.println(m.getKey()+" - "+m.getValue()+ " Shift");
+		} 
+	}
+
+}
+
